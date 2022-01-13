3225ee9e7b28241a8e0ce5bdcfb43ac0e09e04ac
Nguyen Quang Huy <huynq.is@gmail.com>
2022-01-13 14:04:20 +0700

    OK DONE

---
+++ AlsConnect.WebApi/Controllers/UploadController.cs
@@ -0,0 +1,55 @@
+﻿using AlsConnect.Application.Interfaces;
+using AlsConnect.Application.ViewModels.System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace AlsConnect.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class UploadController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _hostingEnvironment;
+
+        public UploadController(IWebHostEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+        [HttpPost("uploadImage")]
+        [AllowAnonymous]
+        public IActionResult UploadImage(IFormFile file)
+        {
+            DateTime now = DateTime.Now;
+            var filename = ContentDispositionHeaderValue
+                                .Parse(file.ContentDisposition)
+                                .FileName
+                                .Trim('"');
+
+            var imageFolder = $@"\uploaded\images\{now.ToString("yyyyMMdd")}";
+
+            string folder = _hostingEnvironment.WebRootPath + imageFolder;
+
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+            string filePath = Path.Combine(folder, filename);
+            using (FileStream fs = System.IO.File.Create(filePath))
+            {
+                file.CopyTo(fs);
+                fs.Flush();
+            }
+            return new OkObjectResult(Path.Combine(imageFolder, filename).Replace(@"\", @"/"));
+
+        }
+    }
+}
